fee6bab9d85ac7a089e51daf2e927d0ffb12b020
J.T. Sage <jtsage+datebox@gmail.com>
2022-06-08 16:20:08 -0400

    When executing with no argument, load the default FS22 log file. Fixes #1
    Thanks @KITT3000

--- logAnalyzer.py
+++ logAnalyzer.py
@@ -32,25 +32,44 @@ print("  logAnalyzer v1.0.2")
 print("    by JTSModding")
 print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n")
 
-parser = argparse.ArgumentParser(description='Grab the interesting bits of a log file')
-parser.add_argument('file', metavar='file', nargs=1, type=argparse.FileType('r', encoding='utf-8'))
+parser = argparse.ArgumentParser(
+    description='Grab the interesting bits of a log file. ' +
+    'Defaults to the log file from your FS2022 install'
+)
+parser.add_argument(
+    'file',
+    metavar='file',
+    nargs='?',
+    type=argparse.FileType('r', encoding='utf-8')
+)
 
 try:
     args = parser.parse_args()
 except BaseException:
     enter_key_exit()
 
+currentFile = None
+
+if args.file is None:
+    currentFile = open(
+        os.getenv("USERPROFILE") + "\Documents\My Games\FarmingSimulator2022\log.txt",
+        "r"
+    )
+else:
+    currentFile = args.file
+
+
 try:
-    thisLogFile = args.file[0].read().split("\n")
-    args.file[0].close()
+    thisLogFile = currentFile.read().split("\n")
+    currentFile.close()
 except BaseException:
-    print("ERROR: Unable to read / parse file '" + os.path.basename(args.file[0].name) + "'")
+    print("ERROR: Unable to read / parse file '" + os.path.basename(currentFile.name) + "'")
     enter_key_exit()
 
 lineNo = 0
 for line in thisLogFile:
     lineNo += 1
-    if "Error:" in line or "Warning:" in line:
+    if "Error:" in line or "Warning:" in line or "ERROR" in line:
         print(str(lineNo) + " :: " + line)
 
 if sys.stdout.isatty():
